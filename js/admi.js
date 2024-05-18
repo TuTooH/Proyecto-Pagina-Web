a8d9514b15c934bfa67417c67c6a749fcae204d4
BlackSaibot <136357589+BlackSaibot@users.noreply.github.com>
2024-05-17 22:28:27 -0400

    Update admi.js

--- js/admi.js
+++ js/admi.js
@@ -1,19 +1,20 @@
-function manejarMantenedorUsuarios() {
-    // Lógica para el mantenedor de usuarios
-    console.log("Manejando el mantenedor de usuarios...");
-}
+$(document).ready(function() {
+    $("form").validate();
 
-function manejarMantenedorProductos() {
-    // Lógica para el mantenedor de productos
-    console.log("Manejando el mantenedor de productos...");
-}
+    $.validator.addMethod("validURL", function(value, element) {
+        return /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i.test(value);
+    }, "Ingrese una URL válida.");
+    
+    $("li.list-group-item").each(function() {
+        $(this).click(function(e) {
+            e.preventDefault(); 
+            var href = $(this).find("a").attr("href");
+            if ($(this).find("a").validURL()) {
+                window.location.href = href;
+            } else {
+                alert("URL inválida.");
+            }
+        });
+    });
+});
 
-function manejarMantenedorVentas() {
-    // Lógica para el mantenedor de ventas
-    console.log("Manejando el mantenedor de ventas...");
-}
-
-function manejarMantenedorBodega() {
-    // Lógica para el mantenedor de bodega
-    console.log("Manejando el mantenedor de bodega...");
-}
